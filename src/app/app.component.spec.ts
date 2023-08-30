96714266fe0b29aad69610c2bf6ccc15906ab726
Jared Bada <official.jared.bada@gmail.com>
2023-08-30 10:03:15 +0300

    project restructure

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'booking-app'`, () => {
+  it(`should have as title 'adebt-free'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('booking-app');
+    expect(app.title).toEqual('adebt-free');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('booking-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('adebt-free app is running!');
   });
 });
