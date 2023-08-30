17a69140f5336636c0f2edaedc98091cfaf97cd3
Jared Bada <official.jared.bada@gmail.com>
2023-08-30 10:12:03 +0300

    refactoring name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'adebt-free'`, () => {
+  it(`should have as title 'booking-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('adebt-free');
+    expect(app.title).toEqual('booking-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('adebt-free app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('booking-app app is running!');
   });
 });
